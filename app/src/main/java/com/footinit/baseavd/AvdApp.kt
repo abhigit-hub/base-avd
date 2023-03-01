5d7d95ace1924824fcded0f8479f802551d5c490
Abhijit Naik <33665662+abhigit-hub@users.noreply.github.com>
2023-03-01 13:28:10 +0530

    Added dependency for Koin & Viewmodel
    Signed-off-by: Abhijit Naik <33665662+abhigit-hub@users.noreply.github.com>

--- app/src/main/java/com/footinit/baseavd/AvdApp.kt
+++ app/src/main/java/com/footinit/baseavd/AvdApp.kt
@@ -1,6 +1,19 @@
 package com.footinit.baseavd
 
 import android.app.Application
+import com.footinit.baseavd.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 class AvdApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@AvdApp)
+            androidLogger()
+            modules(appModule)
+        }
+    }
 }
---
+++ app/src/main/java/com/footinit/baseavd/di/appModules.kt
@@ -0,0 +1,7 @@
+package com.footinit.baseavd.di
+
+import org.koin.dsl.module
+
+val appModule = module {
+
+}
