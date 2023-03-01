d4592c3b8cdc5b4802f2058b8194012063c76196
Abhijit Naik <33665662+abhigit-hub@users.noreply.github.com>
2023-03-01 12:15:13 +0530

    Re-structure gradle project structure
    Signed-off-by: Abhijit Naik <33665662+abhigit-hub@users.noreply.github.com>

--- app/src/main/java/com/footinit/baseavd/ui/MainActivity.kt
+++ app/src/main/java/com/footinit/baseavd/ui/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.footinit.baseavd.ui
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.footinit.baseavd.R
 
 class MainActivity: AppCompatActivity() {
